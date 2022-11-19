1213ecb2375d496d43e599b488c49325de07f2d1
lewfer <lif@arcwave.co.uk>
2022-11-19 09:30:06 +0000

    threading off

--- flask_app.py
+++ flask_app.py
@@ -242,5 +242,5 @@ def controlTestData():
     return redirect(url_for('control'))
 
 if __name__ == '__main__':
-   #app.run(threaded=False, host="0.0.0.0", port=8000)
-   app.run(threaded=True, host="0.0.0.0", port=8000)
+   app.run(threaded=False, host="0.0.0.0", port=8000)
+   #app.run(threaded=True, host="0.0.0.0", port=8000)
